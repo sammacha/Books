60e06ed93981acad1372f3113a18ff8727f7307d
sammacha <samuelnzuki061@gmail.com>
2019-08-01 21:55:17 +0300

    Background threads and API key

--- app/src/main/java/com/example/books/ApiUtil.java
+++ app/src/main/java/com/example/books/ApiUtil.java
@@ -1,6 +1,12 @@
 package com.example.books;
 
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class ApiUtil {
 
@@ -8,18 +14,52 @@ public class ApiUtil {
 
     public static final String  BASE_API_URL =
             "https://www.googleapis.com/books/v1/volumes";
+    public static final String QUERY_PARAMETER_KEY = "q";
+
+    public static final String KEY = "key";
+    public static final String API_KEY = "AIzaSyDZFqAc7xSuTPUoUXlo_O-1TRZvutXTDWI";
 
     public static URL buildUrl(String title){
-        String fullUrl = BASE_API_URL + "?q=" + title;
+
         URL url = null;
+        Uri uri = Uri.parse(BASE_API_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAMETER_KEY,title)
+                .appendQueryParameter(KEY,API_KEY)
+                .build();
 
         try {
-            url = new URL(fullUrl);
+            url = new URL(uri.toString());
         }
         catch (Exception e){
             e.printStackTrace();
         }
 
         return  url;
+    }
+
+    public static String getJson(URL url) throws Exception{
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream stream = connection.getInputStream();
+            Scanner scanner = new Scanner(stream);
+            scanner.useDelimiter("\\A");
+
+            boolean hasData = scanner.hasNext();
+            if (hasData){
+                return scanner.next();
+            }else {
+                return null;
+            }
+        }
+        catch (Exception e){
+            Log.d("Error",e.toString());
+            return  null;
+        }
+
+        finally {
+            connection.disconnect();
+        }
+
     }
 }
